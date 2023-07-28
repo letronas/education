f2a12d7c2e51d6e419ed7d569c4b4dd24bc31327
Letronas <letronas@mail.ru>
2023-07-28 16:32:57 +0300

    Solve 3.1.7.py

---
+++ Stepik/Kind, Kind Python (OOP)/3.1.7.py
@@ -0,0 +1,80 @@
+"""
+Подвиг 7 (на повторение). Объявите класс SmartPhone, объекты которого предполагается создавать командой:
+
+sm = SmartPhone(марка смартфона)
+Каждый объект должен содержать локальные атрибуты:
+
+model - марка смартфона (строка);
+apps - список из установленных приложений (изначально пустой).
+
+Также в классе SmartPhone должны быть объявлены следующие методы:
+
+add_app(self, app) - добавление нового приложения на смартфон (в конец списка apps);
+remove_app(self, app) - удаление приложения по ссылке на объект app.
+
+При добавлении нового приложения проверять, что оно отсутствует в списке apps
+(отсутствует объект соответствующего класса).
+
+Каждое приложение должно определяться своим классом. Для примера объявите следующие классы:
+
+AppVK - класс приложения ВКонтаке;
+AppYouTube - класс приложения YouTube;
+AppPhone - класс приложения телефона.
+
+Объекты этих классов должны создаваться следующим образом (с соответствующим набором локальных атрибутов):
+
+app_1 = AppVK() # name = "ВКонтакте"
+app_2 = AppYouTube(1024) # name = "YouTube", memory_max = 1024
+app_3 = AppPhone({"Балакирев": 1234567890, "Сергей": 98450647365, "Работа": 112}) # name = "Phone",
+phone_list = словарь с контактами
+Пример использования классов (в программе эти строчки не писать):
+
+sm = SmartPhone("Honor 1.0")
+sm.add_app(AppVK())
+sm.add_app(AppVK())  # второй раз добавляться не должно
+sm.add_app(AppYouTube(2048))
+for a in sm.apps:
+    print(a.name)
+
+"""
+class SmartPhone:
+    def __init__(self, model):
+        self.model = model
+        self.apps = []
+    
+    def add_app(self, app):
+        """Добавление нового приложения на смартфон (в конец списка apps)"""
+        if type(app) not in (type(i) for i in self.apps):
+            self.apps.append(app)
+    
+    def remove_app(self, app):
+        """Удаление приложения по ссылке на объект app"""
+        self.apps.remove(app)
+
+
+class AppVK:
+    # класс приложения ВКонтаке;
+    def __init__(self):
+        self.name = "ВКонтакте"
+
+
+class AppYouTube:
+    # класс приложения YouTube;
+    def __init__(self, memory_max):
+        self.name = "YouTube"
+        self.memory_max = memory_max
+
+
+class AppPhone:
+    # класс приложения телефона.
+    def __init__(self, phone_list):
+        self.name = "Phone"
+        self.phone_list = phone_list
+
+
+sm = SmartPhone("Honor 1.0")
+sm.add_app(AppVK())
+sm.add_app(AppVK())  # второй раз добавляться не должно
+sm.add_app(AppYouTube(2048))
+for a in sm.apps:
+    print(a.name)
